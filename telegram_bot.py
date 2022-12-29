228e0bcea88ccfe1310320ced0aa9165e25c5a78
IlNinjaDiBattipaglia <54708940+IlNinjaDiBattipaglia@users.noreply.github.com>
2022-12-29 15:26:24 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,18 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-TOKEN = "5726874288:AAGfl7MKfmFhEQ8x4LkrVmIoQKuRpnH0XE8"
+TOKEN = "Enter your token"
 
 def start(update, context):
-    update.message.reply_text("Bellaaaaaaaaaaa, parole chiave sono vaffanculo, dove sei e che ore sono")
+    update.message.reply_text("Enter the message of the start")
 
 def rispondi(update, context):
     testo = update.message.text.lower()
-    if "vaffanculo" in testo:
-        update.message.reply_text("Ma vacci tu a fare in culo assieme a tua madre")
-    elif "dove sei" in testo:
-        update.message.reply_text("Fatti un poco i cazzi tuoi")
-    elif "che ore sono" in testo:
-        update.message.reply_text("L'ora che ti compri un orologio")
+    if "word" in testo:
+        update.message.reply_text("reply")
+    elif "word" in testo:
+        update.message.reply_text("reply")
+    elif "word" in testo:
+        update.message.reply_text("reply")
 updater = Updater(TOKEN)
 updater.dispatcher.add_handler (CommandHandler('start', start))
 updater.dispatcher.add_handler (MessageHandler(Filters.text, rispondi))
